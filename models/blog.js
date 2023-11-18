e08334ba52dd36fcc9146c08f8caee6d8b25ef47
comdyax <cody24@tuta.io>
2023-11-18 13:11:43 +0100

    added static code analysis tool -> eslint

--- models/blog.js
+++ models/blog.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: String,
-        required: true
-    },
-    url: {
-        type: String,
-        required: true
-    },
-    likes: {
-        type: Number,
-        required: true
-    }
+	title: {
+		type: String,
+		required: true
+	},
+	author: {
+		type: String,
+		required: true
+	},
+	url: {
+		type: String,
+		required: true
+	},
+	likes: {
+		type: Number,
+		required: true
+	}
 })
 
 //TODO: blogSchema format
