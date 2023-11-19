6322f8cc0ef9d7e2268c96c0b22f0c18a88df6d0
comdyax <cody24@tuta.io>
2023-11-19 11:49:57 +0100

    Exc 4.6

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -10,6 +10,42 @@ const listWithOneBlog = [
 		__v: 0
 	}
 ]
+
+const equalCountBlogs = [
+	{
+		_id: "5a422b3a1b54a676234d17f9",
+		title: "Canonical string reduction",
+		author: "Edsger W. Dijkstra",
+		url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+		likes: 12,
+		__v: 0
+	},
+	{
+		_id: '5a422aa71b54a676234d17f8',
+		title: 'Go To Statement Considered Harmful',
+		author: 'Edsger W. Dijkstra',
+		url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+		likes: 5,
+		__v: 0
+	},
+	{
+		_id: "5a422ba71b54a676234d17fb",
+		title: "TDD harms architecture",
+		author: "Robert C. Martin",
+		url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+		likes: 20,
+		__v: 0
+	},
+	{
+		_id: "5a422bc61b54a676234d17fc",
+		title: "Type wars",
+		author: "Robert C. Martin",
+		url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+		likes: 20,
+		__v: 0
+	}
+]
+
 const blogsWithSameLikes = [
 	{
 		_id: "5a422b3a1b54a676234d17f9",
@@ -145,5 +181,32 @@ describe('favorite Blog', () => {
 			likes: 20
 		}
 		expect(listHelper.favoriteBlog(blogsWithSameLikes)).toEqual(result)
+	})
+})
+
+describe('most blogs', () => {
+	test('list of blogs with one author with most blogs', () => {
+		const result = {
+			author: "Robert C. Martin",
+			blogs: 3
+		}
+		expect(listHelper.mostBlogs(blogs)).toEqual(result)
+	})
+	test('empty list', () => {
+		expect(listHelper.mostBlogs([])).toEqual({})
+	})
+	test('list with one element', () => {
+		const result = {
+			author: 'Edsger W. Dijkstra',
+			blogs: 1
+		}
+		expect(listHelper.mostBlogs(listWithOneBlog)).toEqual(result)
+	})
+	test('list with more equal most Blog authors', () => {
+		const result = {
+			author: 'Edsger W. Dijkstra',
+			blogs: 2
+		}
+		expect(listHelper.mostBlogs(equalCountBlogs)).toEqual(result)
 	})
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 const dummy = (blogs) => {
     const result = blogs
     const number = 1
@@ -11,21 +13,47 @@ const totalLikes = (blogs) => {
 }
 
 const favoriteBlog = (blogs) => {
-    if(blogs.length === 0)
+    if (blogs.length === 0)
         return {}
     let highestLikes = 0;
     blogs.forEach(blog => {
         if (blog.likes > highestLikes)
             highestLikes = blog.likes
     })
-    return blogs.map(blog => { 
-        return { 
-            title: blog.title, 
-            author: blog.author, 
-            likes: blog.likes 
-        } 
+    return blogs.map(blog => {
+        return {
+            title: blog.title,
+            author: blog.author,
+            likes: blog.likes
+        }
     })
-    .find(blog => blog.likes === highestLikes)
+        .find(blog => blog.likes === highestLikes)
 }
 
-module.exports = { dummy, totalLikes, favoriteBlog }
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0)
+        return {}
+    //Create (uniq) List of Authors with additional blogs field (initial value 0) 
+    let authors =
+        _.uniq(blogs.map(blog => blog.author))
+            .map(author => {
+                return {
+                    author: author,
+                    blogs: 0
+                }
+            })
+    //Count for each Author the number of blogs and give the value to the field blogs
+    //initialize the max var with the highest blog count
+    let max = 0
+    authors.forEach(author => {
+        author.blogs = blogs.filter(blog =>
+            blog.author === author.author)
+            .length
+        if (author.blogs > max)
+            max = author.blogs
+    })
+    //Return only the Author(Object) with the most blogs (the first, if more than one)
+    return authors.find(author => author.blogs === max)
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs }
