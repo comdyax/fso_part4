edd78a58f93b730703d43f93564b1f719f1d442a
comdyax <cody24@tuta.io>
2023-11-25 22:41:41 +0100

    Exc 4.13 - 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -13,4 +13,20 @@ blogRouter.post('/', async (request, response) => {
     return response.status(201).json(result)
 })
 
+blogRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndDelete(request.params.id)
+    return response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response) => {
+    const updateBlog = {
+        title: request.body.title,
+        author: request.body.author,
+        url: request.body.url,
+        likes: request.body.likes
+    }
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, updateBlog, { new: true })
+    return response.json(updatedBlog)
+})
+
 module.exports = blogRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -13,84 +13,121 @@ beforeEach(async () => {
     await Promise.all(promiseArray)
 })
 
-test('all blogs are returned in correct json format', async () => {
-    const response = await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-    console.log(response.body)
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
+describe('database format', () => {
+    test('all blogs are returned in correct json format', async () => {
+        const response = await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+        console.log(response.body)
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
+
+    test('field id exists', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body[0].id).toBeDefined()
+    })
+
+    test('default likes value', async () => {
+        const newBlog = {
+            title: '2525253 test',
+            author: 'some dude',
+            url: 'some url',
+        }
+        const response = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const blogs = await helper.blogsInDb()
+        const blog = blogs.find(blog => blog.id === response.body.id)
+        expect(blog.likes).toBe(0)
+        expect(response.body.likes).toBe(0)
+    })
 })
 
-test('field id exists', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body[0].id).toBeDefined()
+describe('error testing', () => {
+    test('error message when title field is missing', async () => {
+        const newBlog = {
+            author: 'some dude',
+            url: 'some url',
+        }
+        const response = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+
+        expect(response.body.error).toEqual('Blog validation failed: title: Path `title` is required.')
+    })
+
+    test('error message when url field is missing', async () => {
+        const newBlog = {
+            title: '2525253 test',
+            author: 'some dude',
+        }
+        const response = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+
+        expect(response.body.error).toEqual('Blog validation failed: url: Path `url` is required.')
+    })
 })
 
-test('succesfull post request to db', async () => {
-    const newBlog = {
-        title: '2525253 test',
-        author: 'some dude',
-        url: 'some url',
-        likes: 140
-    }
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+describe('change database', () => {
+    test('succesfull post request to db', async () => {
+        const newBlog = {
+            title: '2525253 test',
+            author: 'some dude',
+            url: 'some url',
+            likes: 140
+        }
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
-    const blogs = await helper.blogsInDb()
-    expect(blogs).toHaveLength(helper.initialBlogs.length + 1)
-    expect(blogs.map(blog => blog.title)).toContain(newBlog.title)
-    expect(blogs.map(blog => blog.author)).toContain(newBlog.author)
-    expect(blogs.map(blog => blog.url)).toContain(newBlog.url)
+        const blogs = await helper.blogsInDb()
+        expect(blogs).toHaveLength(helper.initialBlogs.length + 1)
+        expect(blogs.map(blog => blog.title)).toContain(newBlog.title)
+        expect(blogs.map(blog => blog.author)).toContain(newBlog.author)
+        expect(blogs.map(blog => blog.url)).toContain(newBlog.url)
+    })
+
+    test('deleting a single blog post', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const id = blogsAtStart[0].id
+        await api
+            .delete(`/api/blogs/${id}`)
+            .expect(204)
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+        expect(blogsAtEnd.map(blogs => blogs.id)).not.toContain(id)
+    })
+
+    test('update a single blog post', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const updateBlog = blogsAtStart[0]
+        updateBlog.author = 'new author'
+        updateBlog.url = 'new url'
+        updateBlog.likes = 1
+
+        const response = await api
+            .put(`/api/blogs/${updateBlog.id}`)
+            .send(updateBlog)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        expect(response.body).toEqual(updateBlog)
+        const blogs = await helper.blogsInDb()
+        expect(blogs).toHaveLength(helper.initialBlogs.length)
+        expect(blogs.map(blog => blog.title)).toContain(updateBlog.title)
+        expect(blogs.map(blog => blog.author)).toContain(updateBlog.author)
+        expect(blogs.map(blog => blog.url)).toContain(updateBlog.url)
+    })
 })
-
-test('default likes value', async () => {
-    const newBlog = {
-        title: '2525253 test',
-        author: 'some dude',
-        url: 'some url',
-    }
-    const response = await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-    const blogs = await helper.blogsInDb()
-    const blog = blogs.find(blog => blog.id === response.body.id)
-    expect(blog.likes).toBe(0)
-    expect(response.body.likes).toBe(0)
-})
-
-test('error message when title field is missing', async () => {
-    const newBlog = {
-        author: 'some dude',
-        url: 'some url',
-    }
-    const response = await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-
-    expect(response.body.error).toEqual('Blog validation failed: title: Path `title` is required.')
-})
-
-test('error message when url field is missing', async () => {
-    const newBlog = {
-        title: '2525253 test',
-        author: 'some dude',
-    }
-    const response = await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-
-    expect(response.body.error).toEqual('Blog validation failed: url: Path `url` is required.')
-})
-
 
 
 
